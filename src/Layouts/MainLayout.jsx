1fce3a7256b557863e09db3a938cc75675538721
Redwan islam <redwanislam2760@gmail.com>
2023-05-01 23:12:07 +0600

    navbar added complete

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import Navbar from '../Shared/Navbar'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Navbar from "../Shared/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer";
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
+      <div>
         <Navbar />
+      </div>
+      <div>
         <Outlet />
+      </div>
+      <div className="mt-[auto!important]">
+        <Footer />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 import Home from "../Pages/Home";
+import About from "../Pages/About";
 
 
 const router = createBrowserRouter([
@@ -11,7 +12,11 @@ const router = createBrowserRouter([
             {
                 path:'/',
                 element:<Home />
-            }
+            },
+            {
+                path:'about',
+                element:<About />
+            },
         ]
     }
 ])
