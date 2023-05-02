53cd0dd7d2d017cbcda8d5e2843ac6c5991eb309
Redwan islam <redwanislam2760@gmail.com>
2023-05-02 16:05:59 +0600

    added repies details  page

--- src/Components/ChefDetails.jsx
+++ src/Components/ChefDetails.jsx
@@ -1,14 +1,70 @@
+import { Rating } from '@smastrom/react-rating';
 import React from 'react'
-import { useLoaderData } from 'react-router-dom'
+import { AiFillLike } from 'react-icons/ai';
+import { MdOutlineKeyboardDoubleArrowLeft } from "react-icons/md";
+import { FaQuoteRight } from 'react-icons/fa';
+import { useLoaderData,Link } from 'react-router-dom'
 
 const ChefDetails = () => {
     const data = useLoaderData();
+    
     console.log(data[0]);
-   const {id,image,chef_name,bio,experiences,like, recipe_items}=(data[0]);
+   const {
+     id,
+     image,
+     chef_name,
+     bio,
+     experiences,
+     like,
+     recipe_items,
+     rating,
+     years_of_experience,
+   } = data[0];
 
+
+   const li = recipe_items.map((recipe) => (
+     <li className="bg-gray-100 p-1 rounded-md capitalize">{recipe.name}</li>
+   ));
   return (
-    <div>ChefDetails</div>
-  )
+    <div className="relative">
+      <Link
+        to={`/`}
+        className="p-2 shadow-sm bg-slate-100 rounded-md text-gray-800 mx-auto absolute btn-sm top-2 left-0 font-semibold flex items-center gap-1 sm:left-8 md:left-11 lg:left-16"
+      >
+        <MdOutlineKeyboardDoubleArrowLeft />
+        Go Back
+      </Link>
+      <div className="w-[50%] md:w-[60%] mx-auto border">
+        <div className="relative mx-auto w-full block">
+          <img src={image} alt="" className=" w-full object-cover" />
+          <FaQuoteRight
+            className="absolute left-4 -bottom-2 w-6 h-6 bg-blue-300 p-1 rounded-full"
+            size={10}
+          />
+        </div>
+
+        {/* ==========Content=========== */}
+        <div className="space-y-3 mb-4">
+          <div className="w-24 flex gap-3">
+            <Rating value={rating} readOnly />
+            <span>{rating}</span>
+          </div>
+          <p className="text-gray-600 text-sm sm:text-base">{bio}</p>
+
+          <div>
+            <h1 className="font-medium">List of Recipe</h1>
+            <ul className="flex gap-1">{li}</ul>
+          </div>
+
+          <p className="font-medium">Experience:{years_of_experience} Years</p>
+          <p className="flex items-center gap-1">
+            <AiFillLike size={20} className="text-blue-500" /> {like}
+          </p>
+          <h1 className="text-2xl font-medium">{chef_name}</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ChefDetails
--- src/Components/ChefItem.jsx
+++ src/Components/ChefItem.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import {FaQuoteRight} from 'react-icons/fa'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaQuoteRight } from "react-icons/fa";
 import { Rating } from "@smastrom/react-rating";
-import {AiFillLike} from 'react-icons/ai'
+import { AiFillLike } from "react-icons/ai";
 
-export const ChefItem = ({ id, image, chef_name, bio,   years_of_experience,rating,like }) => {
-
+export const ChefItem = ({
+  id,
+  image,
+  chef_name,
+  bio,
+  years_of_experience,
+  rating,
+  like,
+  recipe_items
+}) => {
+    const li = recipe_items.map((recipe) => <li className="bg-gray-100 p-1 rounded-md capitalize">{recipe.name}</li>);
 
   return (
-    <div className="shadow-md p-4">
-      <div className="grid items-center gap-3 sm:gap-1 sm:grid-cols-[100px,_1fr]">
+    <div className="shadow-md p-4 flex flex-col">
+      <div className="grid gap-3 sm:gap-1 sm:grid-cols-[100px,_1fr]">
         <div className="relative w-20 h-20 mx-auto sm:mx-0">
           <img
             src={image}
@@ -23,19 +32,27 @@ export const ChefItem = ({ id, image, chef_name, bio,   years_of_experience,rati
         </div>
 
         {/* ==========Content=========== */}
-        <div className='space-y-3'>
+        <div className="space-y-3 mb-4">
           <div className="w-24 flex gap-3">
             <Rating value={rating} readOnly />
             <span>{rating}</span>
           </div>
           <p className="text-gray-600 text-sm sm:text-base">{bio}</p>
-          <p className='font-medium'>Experience:{years_of_experience} Years</p>
-          <p className='flex items-center gap-1'><AiFillLike size={20} className='text-blue-500'/> {like}</p>
+
+          <div>
+            <h1 className="font-medium">List of Recipe</h1>
+            <ul className="flex gap-1">{li}</ul>
+          </div>
+
+          <p className="font-medium">Experience:{years_of_experience} Years</p>
+          <p className="flex items-center gap-1">
+            <AiFillLike size={20} className="text-blue-500" /> {like}
+          </p>
           <h1 className="text-2xl font-medium">{chef_name}</h1>
         </div>
       </div>
 
-      <Link to={`/chefItem/${id}`} className="btn w-full">
+      <Link to={`/chefItem/${id}`} className="btn w-full mt-[auto!important]">
         View Recipes
       </Link>
     </div>
