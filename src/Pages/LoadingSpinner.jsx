7af8ff21fd15647b436012945d2b2a2afab672d0
Redwan islam <redwanislam2760@gmail.com>
2023-05-04 07:25:47 +0600

    addd loading spiner elment

---
+++ src/Pages/LoadingSpinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Lottie from "lottie-react";
+import loading from "../../public/loading-animation.json";
+const LoadingSpinner = () => {
+  return (
+    <div className='min-h-screen w-full'>
+      <div className="max-w-md mx-auto">
+        <Lottie animationData={loading} loop={true} />
+      </div>
+    </div>
+  );
+}
+
+export default LoadingSpinner
