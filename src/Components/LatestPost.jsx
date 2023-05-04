8530696c0b15597448cbf2f153d96ec327a44b44
Redwan islam <redwanislam2760@gmail.com>
2023-05-04 09:35:26 +0600

    addd latest post component

---
+++ src/Components/LatestPost.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import SectionTitle from '../Shared/SectionTitle';
+import { FaCalendar, FaCalendarAlt, FaComment } from 'react-icons/fa';
+
+// ========= posts data =========
+const posts = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/4552047/pexels-photo-4552047.jpeg?auto=compress&cs=tinysrgb&w=600",
+    title: "Top 20 rustic fruit pie recipes",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum odio iste, ipsum provident ullam aliquid animi voluptas corporis, quos natus",
+    date: "march 13, 2023",
+    comment: 351,
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/6287250/pexels-photo-6287250.jpeg?auto=compress&cs=tinysrgb&w=400",
+    title: "Make your own healthy and delicious jams",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum odio iste, ipsum provident ullam aliquid animi voluptas corporis, quos natus",
+    date: "january 13, 2023",
+    comment: 412,
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/7474291/pexels-photo-7474291.jpeg?auto=compress&cs=tinysrgb&w=400",
+    title: "Top 20 rustic fruit pie recipes",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum odio iste, ipsum provident ullam aliquid animi voluptas corporis, quos natus",
+    date: "December 13, 2022",
+    comment: 689,
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/4940755/pexels-photo-4940755.jpeg?auto=compress&cs=tinysrgb&w=600",
+    title: "Host a feast in your backyard",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum odio iste, ipsum provident ullam aliquid animi voluptas corporis, quos natus",
+    date: "December 13, 2022",
+    comment: 220,
+  },
+  {
+    id: 5,
+    img: "https://images.pexels.com/photos/4040695/pexels-photo-4040695.jpeg?auto=compress&cs=tinysrgb&w=600",
+    title: "Host a feast in your backyard",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum odio iste, ipsum provident ullam aliquid animi voluptas corporis, quos natus",
+    date: "December 13, 2022",
+    comment: 120,
+  },
+  {
+    id: 6,
+    img: "https://images.pexels.com/photos/7525161/pexels-photo-7525161.jpeg?auto=compress&cs=tinysrgb&w=600",
+    title: "Host a feast in your backyard",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum odio iste, ipsum provident ullam aliquid animi voluptas corporis, quos natus",
+    date: "December 13, 2022",
+    comment: 620,
+  },
+];
+const LatestPost = () => {
+  return (
+    <div className='section'>
+      <div className='my-8'>
+        <SectionTitle title={"Latest Posts"} />
+      </div>
+      <div className='grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        {posts.map((post) => (
+          <div key={post.id} className="card bg-base-100 shadow-xl">
+            <figure>
+              <img src={post.img} alt="recipe" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">
+               {post.title}
+              </h2>
+              <hr />
+
+              <div className='flex justify-between items-center'>
+                <div className='flex items-center gap-1'>
+                    <FaCalendarAlt opacity={.7}/>
+                    <span>{post.date}</span>
+                </div>
+                <div className='flex items-center gap-1'>
+                    <FaComment opacity={.7}/>
+                    <span>{post.comment}</span>
+                </div>
+              </div>
+              <hr />
+
+                <p>{post.desc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default LatestPost
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Slider from '../Shared/Slider/Slider'
 import Chef from './Chef'
 import LatestRecipe from '../Components/Recipe/LatestRecipe'
+import LatestPost from '../Components/LatestPost'
 
 const Home = () => {
 
@@ -13,6 +14,9 @@ const Home = () => {
         </div>
         <div>
         <Chef />
+        <div>
+          <LatestPost />
+        </div>
         </div>
     </div>
   )
